b94c8f116405cf46029b1099cad8887c68bf6427
Harrison Walters <famishedmammal@gmail.com>
2016-02-27 16:59:29 -0800

    Update lobbyserverTEST.cs

--- lobbyserverTEST.cs
+++ lobbyserverTEST.cs
@@ -2,21 +2,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using Steamworks;
 using System.Collections;
+using System.Collections.Generic;
 
 public class lobbyserverTEST : MonoBehaviour {
 
     protected Callback<LobbyCreated_t> Callback_lobbyCreated;
     protected Callback<LobbyMatchList_t> Callback_lobbyList;
     protected Callback<LobbyEnter_t> Callback_lobbyEnter;
+    protected Callback<LobbyDataUpdate_t> Callback_lobbyInfo;
 
-    ulong lobbyID;
+    ulong current_lobbyID;
+    List<CSteamID> lobbyIDS;
 
     // Use this for initialization
     void Start () {
 
+        lobbyIDS = new List<CSteamID>();
         Callback_lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
         Callback_lobbyList = Callback<LobbyMatchList_t>.Create(OnGetLobbiesList);
         Callback_lobbyEnter = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+        Callback_lobbyInfo = Callback<LobbyDataUpdate_t>.Create(OnGetLobbyInfo);
 
         if (SteamAPI.Init())
             Debug.Log("Steam API init -- SUCCESS!");
@@ -52,12 +57,12 @@ public class lobbyserverTEST : MonoBehaviour {
         // Command - List lobby members
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            int numPlayers = SteamMatchmaking.GetNumLobbyMembers((CSteamID)lobbyID);
+            int numPlayers = SteamMatchmaking.GetNumLobbyMembers((CSteamID)current_lobbyID);
 
             Debug.Log("\t Number of players currently in lobby : " + numPlayers);
             for (int i = 0; i < numPlayers; i++)
             {
-                Debug.Log("\t Player(" + i + ") == " + SteamFriends.GetFriendPersonaName(SteamMatchmaking.GetLobbyMemberByIndex((CSteamID)lobbyID, i)));
+                Debug.Log("\t Player(" + i + ") == " + SteamFriends.GetFriendPersonaName(SteamMatchmaking.GetLobbyMemberByIndex((CSteamID)current_lobbyID, i)));
             }
         }
     }
@@ -68,16 +73,37 @@ public class lobbyserverTEST : MonoBehaviour {
             Debug.Log("Lobby created -- SUCCESS!");
         else
             Debug.Log("Lobby created -- failure ...");
+
+        SteamMatchmaking.SetLobbyData((CSteamID)result.m_ulSteamIDLobby, "name", "A game!");
     }
 
     void OnGetLobbiesList(LobbyMatchList_t result)
     {
         Debug.Log("Found " + result.m_nLobbiesMatching + " lobbies!");
+        for(int i=0; i< result.m_nLobbiesMatching; i++)
+        {
+            CSteamID lobbyID = SteamMatchmaking.GetLobbyByIndex(i);
+            lobbyIDS.Add(lobbyID);
+            SteamMatchmaking.RequestLobbyData(lobbyID);
+        }
+    }
+
+    void OnGetLobbyInfo(LobbyDataUpdate_t result)
+    {
+        for(int i=0; i<lobbyIDS.Count; i++)
+        {
+            if (lobbyIDS[i].m_SteamID == result.m_ulSteamIDLobby)
+            {
+                Debug.Log("Lobby " + i+" :: " +SteamMatchmaking.GetLobbyData((CSteamID)lobbyIDS[i].m_SteamID, "name"));
+                return;
+            }
+        }
+       
     }
 
     void OnLobbyEntered(LobbyEnter_t result)
     {
-        lobbyID = result.m_ulSteamIDLobby;
+        current_lobbyID = result.m_ulSteamIDLobby;
 
         if (result.m_EChatRoomEnterResponse == 1)
             Debug.Log("Lobby joined!");
